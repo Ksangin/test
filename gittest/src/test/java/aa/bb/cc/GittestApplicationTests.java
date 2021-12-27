91e2dbbf1e6d2ccacfcde369c3f180146063f11f
SangInKim <SangInKim@DESKTOP-MFBM613>
2021-12-27 17:45:59 +0900

    commit test

---
+++ gittest/src/test/java/aa/bb/cc/GittestApplicationTests.java
@@ -0,0 +1,13 @@
+package aa.bb.cc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
